e3ec6520df470413417146285fd6064a5c8c7cca
AlejandroBass7 <bassvidal07@gmail.com>
2026-05-10 22:57:04 -0500

    Avance CRUD

--- app/src/main/java/com/example/ahorrify/Ingreso.java
+++ app/src/main/java/com/example/ahorrify/Ingreso.java
@@ -1,17 +1,14 @@
 package com.example.ahorrify;
 
 public class Ingreso {
-    private int id; // Nuevo campo
     private String categoria;
     private double monto;
 
-    public Ingreso(int id, String categoria, double monto) {
-        this.id = id;
+    public Ingreso(String categoria, double monto) {
         this.categoria = categoria;
         this.monto = monto;
     }
 
-    public int getId() { return id; }
     public String getCategoria() { return categoria; }
     public double getMonto() { return monto; }
 }
